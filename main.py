97c40146dfdc2f7b58ab07b1cdda7607256ca32e
elijah <cuiyeqing960904@gmail.com>
2025-12-05 11:59:35 +0300

    embeddings + text loader + searching , building prompt

--- main.py
+++ main.py
@@ -1,3 +1,34 @@
 from dotenv import load_dotenv
+from langchain_community.document_loaders import TextLoader
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
+from langchain_text_splitters import CharacterTextSplitter
 
 load_dotenv()
+
+embeddings = OpenAIEmbeddings()
+
+
+text_splitter = CharacterTextSplitter(
+    separator="\n",
+    chunk_size=200,
+    chunk_overlap=0
+)
+
+loader = TextLoader("facts.txt")
+docs = loader.load_and_split(
+    text_splitter=text_splitter
+)
+
+db = Chroma.from_documents(
+    docs,
+    embedding=embeddings,
+    persist_directory="emb"
+)
+
+results = db.similarity_search_with_score("what is an interesting fact about the english language ?")
+
+for result in results:
+    print("\n")
+    print(result[1])
+    print(result[0].page_content)
